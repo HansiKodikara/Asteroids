06fe74059c1689f60f10a5203422483c211a5800
HansiKodikara <hansikodikara7@gmail.com>
2025-01-07 17:55:15 +0530

    refactor: implement pygame groups for game object management

--- main.py
+++ main.py
@@ -5,16 +5,23 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     clock = pygame.time.Clock()
-    player_object = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+    player_object = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player_object.update(dt)
-        player_object.draw(screen)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt_ms = clock.tick(60)
         dt = dt_ms/1000
