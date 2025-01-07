8c2d818d41b53e305c6887f02f50a6914dd1e9b5
HansiKodikara <hansikodikara7@gmail.com>
2025-01-07 06:55:03 +0530

    feat: draw player spaceship

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player_object = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        screen.fill("black")
+        player_object.draw(screen)
         pygame.display.flip()
+        dt_ms = clock.tick(60)
+        dt = dt_ms/1000
 
 if __name__ == "__main__":
     main()
