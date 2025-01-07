ddc59aa7b687c8b9416a4bf28c357743321f1786
HansiKodikara <hansikodikara7@gmail.com>
2025-01-07 20:11:37 +0530

    feat: add Asteroid class with movement and spawning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -11,8 +13,12 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable,drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
     player_object = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    AsteroidField_object = AsteroidField()
 
     while True:
         for event in pygame.event.get():
